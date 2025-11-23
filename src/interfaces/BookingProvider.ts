7424dffbfeca68e0954eeeca69f6bab44f0fa042
yoav.weber <yoav.weber@code.berlin>
2025-11-22 21:22:27 -0300

    remove irrelevant code due to erro rin folder

--- src/interfaces/BookingProvider.ts
+++
@@ -1,8 +0,0 @@
-import { Listing, SearchCriteria, BookingDetails, BookingResult } from '../schemas/bookingSchemas.js';
-
-export { Listing, SearchCriteria, BookingDetails, BookingResult };
-
-export interface BookingProvider {
-    searchListings(criteria: SearchCriteria): Promise<Listing[]>;
-    createBooking(bookingDetails: BookingDetails): Promise<BookingResult>;
-}
